9122ff2dfbffcfb279a72b3df9d1390ef46f9bbd
Arthur Lozano <chillout885@gmail.com>
2021-03-31 21:38:45 -0700

    driver

---
+++ driver.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+const io = require('socket.io-client');
+const host = 'http://localhost:3331';
+const caps = io.connect(host);
+
+
+caps.on('driverpickup', pickUp);
+
+
+// events.on('inTransit', inTransit);
+
+function pickUp(payload) {
+  console.log('Package awaiting delivery');
+  setTimeout(() => {
+    console.log('picking up', payload.orderId);
+    caps.emit('in-transit', payload);
+  }, 1500)
+  setTimeout(() => {
+    caps.emit('delivered', payload);
+  }, 3000)
+}
+
+
